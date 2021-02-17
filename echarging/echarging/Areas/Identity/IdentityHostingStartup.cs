68fe8c0fb9caeac3e1f0d1c44b7049498ae750c0
Kasper <kmadse16@student.aau.dk>
2021-02-17 12:32:43 +0100

    Identity added

---
+++ echarging/echarging/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using echarging.Areas.Identity.Data;
+using echarging.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(echarging.Areas.Identity.IdentityHostingStartup))]
+namespace echarging.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<echargingContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("echargingContextConnection")));
+
+                services.AddDefaultIdentity<echargingUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<echargingContext>();
+            });
+        }
+    }
+}
