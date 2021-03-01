36dbe6de41d2c1f1d3a6662059f61e52dd84a171
Kasper Holst Hansen <khha18@student.aau.dk>
2021-03-01 15:14:15 +0100

    fix

--- echarging/echarging/Pages/Opensource.cshtml.cs
+++ echarging/echarging/Pages/Opensource.cshtml.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace echarging.Pages
 {
     public class OpensourceModel : PageModel
     {
+        private readonly ILogger<OpensourceModel> _logger;
+
+        public OpensourceModel(ILogger<OpensourceModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
-
         }
     }
 }
