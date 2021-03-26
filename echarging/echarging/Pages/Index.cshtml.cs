16913da431d0b9d4ed54b732e4eab791b1956128
Kasper Madsen <kmadse16@student.aau.dk>
2021-03-26 16:08:32 +0100

    routing test

--- echarging/echarging/Pages/Index.cshtml.cs
+++ echarging/echarging/Pages/Index.cshtml.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.IO;
+using Itinero;
+using Itinero.IO.Osm;
+using Itinero.Osm.Vehicles;
 
 namespace echarging.Pages
 {
@@ -16,10 +20,39 @@ namespace echarging.Pages
         {
             _logger = logger;
         }
+        
 
         public void OnGet()
         {
+        }
+        /*
+        public IActionResult OnPostGo()
+        {
+            var routerDb = RouterDb.Deserialize(stream, RouterDbProfile.NoCache);
+            routerDb.AddContracted(routerDb.GetSupportedProfile("car"));
 
+
+            return Page();
+        }
+        */
+
+        public IActionResult OnPostGo()
+        {
+            // load some routing data and build a routing network.
+            var routerDb = new RouterDb();
+            using (var stream = new FileInfo(@"/root/osm/denmark-latest.osm.pbf").OpenRead())
+            {
+                // create the network for cars only.
+                routerDb.LoadOsmData(stream, Vehicle.Car); 
+            }
+
+            // write the routerdb to disk.
+            using (var stream = new FileInfo(@"/root/osm/output/routing.routerdb").Open(FileMode.Create))
+            {
+                routerDb.Serialize(stream);
+            }
+
+            return Page(); 
         }
     }
 }
