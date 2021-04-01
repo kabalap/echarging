5911abb668ff59e31cf30c8761de04a8fce371c5
Kasper Madsen <kmadse16@student.aau.dk>
2021-04-01 14:24:56 +0200

    min fejl

--- echarging/echarging/Startup.cs
+++ echarging/echarging/Startup.cs
@@ -22,7 +22,7 @@ namespace echarging
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-         
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
