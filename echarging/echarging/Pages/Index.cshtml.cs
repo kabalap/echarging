8bb485ace2c43e57f0a77e54da3b11d0c8fb41dc
Kasper Madsen <kmadse16@student.aau.dk>
2021-04-13 12:16:37 +0200

    hejhejanders

--- echarging/echarging/Pages/Index.cshtml.cs
+++ echarging/echarging/Pages/Index.cshtml.cs
@@ -10,6 +10,8 @@ using Itinero;
 using Itinero.IO.Osm;
 using Itinero.Osm.Vehicles;
 
+
+
 namespace echarging.Pages
 {
     public class IndexModel : PageModel
@@ -38,22 +40,56 @@ namespace echarging.Pages
 
         public IActionResult OnPostGo()
         {
+            /*
             // load some routing data and build a routing network.
             var routerDb = new RouterDb();
-            using (var stream = new FileInfo(@"/root/osm/denmark-latest.osm.pbf").OpenRead())
+            using (var stream = new FileInfo(@"Users/Kasper/Desktop/osm/denmark-latest.osm.pbf").OpenRead())
             {
                 // create the network for cars only.
                 routerDb.LoadOsmData(stream, Vehicle.Car); 
             }
+*/
 
             // write the routerdb to disk.
-            using (var stream = new FileInfo(@"/root/osm/output/routing.routerdb").Open(FileMode.Create))
+            using (var stream = new FileInfo(@"Users/Kasper/Desktop/osm/output/routing.routerdb").Open(FileMode.Create))
             {
-                routerDb.Serialize(stream);
+                var routerDb = new RouterDb();
+                    routerDb.Serialize(stream);
             }
+            
+            
 
             return Page(); 
             
+        }
+
+        public IActionResult OnPostWin()
+        {
+            // load some routing data and build a routing network.
+            var routerDb = new RouterDb();
+            using (var stream = new FileInfo(@"/Users/Kasper/Desktop/osm/output/routing.routerdb").OpenRead())
+            {
+                routerDb = RouterDb.Deserialize(stream, RouterDbProfile.NoCache); // create the network for cars only.
+            }
+
+            // create a router.
+            var router = new Router(routerDb);
+
+// get a profile.
+            var profile = Vehicle.Car.Fastest(); // the default OSM car profile.
+
+// create a routerpoint from a location.
+// snaps the given location to the nearest routable edge.
+            var start = router.Resolve(profile, 51.26797020271655f, 4.801905155181885f);
+            var end = router.Resolve(profile, 52.26797020271655f, 4.801005155181885f);
+
+// calculate a route.
+            var route = router.Calculate(profile, start, end);
+            using (var stream = new FileInfo(@"/Users/Kasper/Desktop/osm/output/routing.routerdb").OpenRead())
+            {
+                route.ToGeoJson();
+            }
+            return null;
         }
     }
 }
